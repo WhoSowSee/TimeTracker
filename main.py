eef824ccd543cdf5e2aa06ccb3ff87977b6d1f99
GodOfTheNewWorlld <yanmishbash.i.s.2.21@gmail.com>
2024-03-18 21:34:26 +0300

    Added progress bar to main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import sys
+import time
 
 from tqdm import tqdm
 
@@ -9,61 +10,69 @@ from utils import green, red, white
 
 # Если система Windows -> cls, иначе -> clear
 def clear_screen():
-	os.system("cls" if os.name == "nt" else "clear")
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def date_request() -> int:
-	options = (
-		"Добавить занятия",
-		"Посмотреть активность за последние две недели",
-		"Посмотреть активность за все время",
-		"Посмотреть среднее время активности по неделям",
-		"Посмотреть активность в виде круга",
-		"Завершить сессию",
-	)
+    options = (
+        'Добавить занятия',
+        'Посмотреть активность за последние две недели',
+        'Посмотреть активность за все время',
+        'Посмотреть среднее время активности по неделям',
+        'Посмотреть активность в виде круга',
+        'Завершить сессию',
+    )
 
-	while True:
-		for ind, name in enumerate(options, start=1):
-			print(f"{green}{ind}{white}: {name}")
+    while True:
+        for ind, name in enumerate(options, start=1):
+            print(f'{green}{ind}{white}: {name}')
 
-		try:
-			activity_operation = int(input("\nВвод: "))
-			if activity_operation in (1, 2, 3, 4, 5, 6):
-				return activity_operation
-			else:
-				raise ValueError
-		except ValueError:
-			input(f"\n{red}Неверный ввод{white}")
-			clear_screen()
+        try:
+            activity_operation = int(input('\nВвод: '))
+            if activity_operation in (1, 2, 3, 4, 5, 6):
+                return activity_operation
+            else:
+                raise ValueError
+        except ValueError:
+            input(f'\n{red}Неверный ввод{white}')
+            clear_screen()
 
 
 def file_call(file_path: str) -> None:
-	subprocess.run(['python', file_path])
-	clear_screen()
+    with tqdm(total=100, desc='Генерация изображения') as f:
+        for i in range(5):
+            time.sleep(0.4)
+            f.update(20)
+    subprocess.run(['python', file_path])
 
-def basic_logic() -> None:
-	file_paths = {
-		1: "tracker.py",
-		2: "bar.py",
-		3: "map.py",
-		4: "density.py",
-		5: "circles.py",
-	}
-	while True:
-		activity_operation: int = date_request()
+    clear_screen()
 
-		if activity_operation in file_paths:
-			file_call(file_paths.get(activity_operation))
-		elif activity_operation == 6:
-			clear_screen()
-			sys.exit()
+
+def run_activity() -> None:
+    file_paths = {
+        2: 'bar.py',
+        3: 'map.py',
+        4: 'density.py',
+        5: 'circles.py',
+    }
+    while True:
+        activity_operation: int = date_request()
+
+        if activity_operation in file_paths:
+            file_call(file_paths.get(activity_operation))
+        elif activity_operation == 6:
+            clear_screen()
+            sys.exit()
+        elif activity_operation == 1:
+            subprocess.run(['python', 'tracker.py'])
+            clear_screen()
 
 
 def main():
-	while True:
-		clear_screen()
-		basic_logic()
+    while True:
+        clear_screen()
+        run_activity()
 
 
-if __name__ == "__main__":
-	main()
+if __name__ == '__main__':
+    main()
